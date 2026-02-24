b88f0bb9e489097b0cc15db7d62148358bf65bb8
Jorge Valdez <jorge@yung.dev>
2026-02-24 18:05:04 -0500

    fix types

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,3 +1,4 @@
+---@module "wezterm.types.wezterm"
 local wezterm = require("wezterm") ---@type Wezterm
 local appearance = require("appearance")
 local config = wezterm.config_builder() ---@type Config
