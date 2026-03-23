8d0e0c343c5e0dd15834d9304bc3961c42e973f3
Jorge Valdez <jorge@yung.dev>
2026-03-23 16:12:27 -0700

    add pr list for myself commands

--- zsh/pr.zsh
+++ zsh/pr.zsh
@@ -3,3 +3,13 @@
 function prm() {
     pi -p "open a pull request for the jj bookmark '$1' that's already been pushed to github, and give me the url. there's no jira ticket. set me as the assignee."
 }
+
+function prs() {
+    REPO=$(jj git remote list | cut -d" " -f 2 | sed 's/.*github\.com\/\(.*\)\.git/\1/g')
+    gh pr list -A '@me' -R "$REPO" --state open
+}
+
+function reponame() {
+    REPO=$(jj git remote list | cut -d" " -f 2 | sed 's/.*github\.com[\/:]\(.*\)\.git/\1/g')
+    echo "$REPO"
+}
